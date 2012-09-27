9f49d534ef900defa0365cd1fc2b9597233fba18
tokuhirom <tokuhirom@gmail.com>
2012-09-27 09:46:06 +0900

    initial import

---
+++ lib/Acme/Module/Checker.pm
@@ -0,0 +1,39 @@
+package Acme::Module::Checker;
+use strict;
+use warnings;
+use 5.008008;
+our $VERSION = '0.01';
+
+
+
+1;
+__END__
+
+=encoding utf8
+
+=head1 NAME
+
+Acme::Module::Checker - A module for you
+
+=head1 SYNOPSIS
+
+  use Acme::Module::Checker;
+
+=head1 DESCRIPTION
+
+Acme::Module::Checker is
+
+=head1 AUTHOR
+
+Tokuhiro Matsuno E<lt>tokuhirom AAJKLFJEF@ GMAIL COME<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+Copyright (C) Tokuhiro Matsuno
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
